e07e767c09a01743184e27ae077cb76d15680a7e
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2021-11-09 19:57:01 +0200

    Change the image when the button is clicked

--- app/src/main/java/com/example/hw1/MainActivity.kt
+++ app/src/main/java/com/example/hw1/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.hw1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    var androidIsGreen = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun changeImage(view: android.view.View) {}
+    fun changeImage(view: android.view.View) {
+        val image = findViewById<ImageView>(R.id.imageView)
+
+        if(androidIsGreen)
+        {
+            image.setImageResource(R.drawable.android_black)
+            androidIsGreen = false;
+        }
+        else
+        {
+            image.setImageResource(R.drawable.android_green)
+            androidIsGreen = true;
+        }
+    }
 }
